d54bc20c4b24afdf7b84220e33d9a7f0f67cf01c
“Rohan <rohanhasabe8@gmail.com>
2021-05-22 19:30:33 +0200

    sdm660-common: Use legacy power

--- common.mk
+++ common.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service \
     vendor.qti.hardware.perf@2.0.vendor
 
 # Protobuf
